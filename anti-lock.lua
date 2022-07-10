8c930511f820e2d187892da56d2a8b51f530b827
zfnxx <97049846+zfnxx@users.noreply.github.com>
2022-07-10 16:06:47 +0200

    Create anti-lock.lua

---
+++ anti-lock.lua
@@ -0,0 +1,46 @@
+if getgenv().antilockh then
+    return
+end
+getgenv().antilockh = true
+local UIS = game:GetService("UserInputService")
+local Antilocking = false
+local Animate = game.Players.LocalPlayer.Character.Animate
+local Idle1 = Animate.idle.Animation1.AnimationId
+local Idle2 = Animate.idle.Animation2.AnimationId
+local Walkspeed = 0.0122
+
+loadstring(game:HttpGet("https://www.klgrth.io/paste/85rzj/raw"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zfnxx/Da-Hood-Anti-Cheat-Bypass/main/anti-cheat-bypass.lua"))()
+
+UIS.InputBegan:Connect(function(input, pressed)
+    if pressed == false and input.KeyCode == Enum.KeyCode.Z then
+        if Antilocking == false then
+            Antilocking = true
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "oxz#0001 anti lock",
+                Text = "Enabled",
+                Duration = 1,
+            })
+        else
+            Antilocking = false
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "oxz#0001 anti lock",
+                Text = "Disabled",
+                Duration = 1,
+            })
+        end
+    end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    if Antilocking == true then
+        Animate.idle.Animation1.AnimationId = Animate.run.RunAnim.AnimationId
+        Animate.idle.Animation2.AnimationId = Animate.run.RunAnim.AnimationId
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.Humanoid.MoveDirection * 0.0115
+    else
+        Animate.idle.Animation1.AnimationId = Idle1
+        Animate.idle.Animation2.AnimationId = Idle2
+    end
+end)
